c6f70990b207c5e6f65bcb18b127ab63960ee392
FavoriDeLaFortune <favdela4tune@gmail.com>
2021-10-08 08:58:04 +0300

    all done

--- src/com/company/Main_156.java
+++ src/com/company/Main_156.java
@@ -2,16 +2,15 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main_156 {
 
     public static void main(String[] args) {
-        int x, y, z, w;
+        int x, y, z;
         Scanner in = new Scanner(System.in);
         x = in.nextInt();
         y = in.nextInt();
         z = in.nextInt();
-        w = in.nextInt();
-        if (x == -y || x == -z || x == -w || y == -z || y == -w || z == -w) {
+        if (x % 2 == 0 && y % 2 == 0 || x % 2 == 0 && z % 2 == 0 || y % 2 == 0 && z % 2 == 0) {
             System.out.println("true");
         } else {
             System.out.println("false");
